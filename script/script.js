a100456e81fc77ee26cee63b6701f55f4d92bc86
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-11-04 07:39:47 +0800

    feat(script.js): add modal window

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  // Timer
+
   const countTimer = (deadline) => {
     const timerHours = document.querySelector('#timer-hours'),
           timerMinutes = document.querySelector('#timer-minutes'),
@@ -33,15 +35,68 @@ window.addEventListener('DOMContentLoaded', () => {
       trueClock(timer.minutes, timerMinutes);
       trueClock(timer.seconds, timerSeconds);
       
-      
       if (timer.timeRemaining <= 0) {
         clearInterval(timeInterval);
-      }
-      
+      } 
     }
-
   };
+  countTimer('06 nov 2020');
 
-  countTimer('03 nov 2020');
+
+  // Menu
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+          menu = document.querySelector('menu'),
+          closeBtn = document.querySelector('.close-btn'),
+          menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((item) => item.addEventListener('click', handlerMenu));
+  };
+  toggleMenu();
+
+
+  //POPUP Window
+  const togglePopUp = () => {
+    let requestId;
+
+    const popup = document.querySelector('.popup'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popupCloseBtn = popup.querySelector('.popup-close');
+
+    const animation = () => {
+      requestId = requestAnimationFrame(animation);
+      let value = 0;
+      value = parseFloat(popup.style.opacity);
+
+      if (value > 1) { cancelAnimationFrame(requestId); }
+
+      value += 0.015;
+      popup.style.opacity = value;
+    };
+
+    popupBtn.forEach(item => {
+      item.addEventListener('click', () => {
+        if (window.innerHeight > 768) {
+          popup.style.opacity = 0;
+          popup.style.display = 'block';
+          requestId = requestAnimationFrame(animation);
+        } else {
+          popup.style.display = 'block';
+        }
+      });
+    });
+
+    popupCloseBtn.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+  };
+  togglePopUp();
+
 
 });
