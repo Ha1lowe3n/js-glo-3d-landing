d5843356988335ebfc1a143924a6fc9fc7a079eb
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-11-11 14:25:40 +0800

    feat(script.js): add calc and delete func calc RegExp(functional added in calc

--- script/script.js
+++ script/script.js
@@ -307,20 +307,53 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
   // calc
-  const calcNumbers = () => {
-    const calcBlock = document.querySelector('.calc-block');
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+          calcType = document.querySelector('.calc-type'),
+          calcSquare = document.querySelector('.calc-square'),
+          calcDay = document.querySelector('.calc-day'),
+          calcCount = document.querySelector('.calc-count'),
+          totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+          countValue = 1,
+          dayValue = 1;
+
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+            squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+   
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      } 
+
+      totalValue.textContent = total;
+    };
 
     const validate = (target) => target = target.replace(/\D/g, '');
 
     calcBlock.addEventListener('input', (e) => {
       const target = e.target;
 
-      if (target.matches('.calc-square') || target.matches('.calc-count') || 
-      target.matches('.calc-day')) {
+      if (target.matches('select') || target.matches('input')) {
         target.value = validate(target.value);
+      }
+
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
       }
     });
   };
-  calcNumbers();
+  calc(100);
 
 });
